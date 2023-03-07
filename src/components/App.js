49d3bc978d680c2ee09f67b6116322dbe5620a3b
Bradbuchholz <bradbuchholz@gmail.com>
2023-03-06 20:02:33 -0800

    update file structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
+import About from './About';
+import Projects from './Projects';
+import Skills from './Skills';
+import Contact from './Contact';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
     </React.Fragment>
   );
 }
