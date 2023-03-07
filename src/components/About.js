005e2c26c3e9c02ec5cf9446428724ff670693bb
Bradbuchholz <bradbuchholz@gmail.com>
2023-03-06 20:32:44 -0800

    update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 function About() {
-
+  return (
+    <section id='about'>
+      <div className=''>
+        <div className=''>
+          <h1 className=''>Hi, I'm Brad
+            <br className='hidden lg: inline-block' />I am a something something something fill in later.
+          </h1>
+          <p className='mb-8 leading-relaxed'>
+            Insert text later.
+          </p>
+          <div className='flex justify-center'>
+            <a
+              href='#contact'
+              className='inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg'>
+                Get in touch with me! 
+              </a>
+              <a
+                href='#projects'
+                className='ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg'>
+                Check out some of my past work
+              </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default About;
