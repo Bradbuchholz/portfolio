c67ad20ac0bc4557e95985bf0c6ed982cb466eca
Bradbuchholz <bradbuchholz@gmail.com>
2023-03-06 20:07:43 -0800

    update multiple components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navbar from './Navbar';
-import About from './About';
-import Projects from './Projects';
-import Skills from './Skills';
-import Contact from './Contact';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 function App() {
   return (
